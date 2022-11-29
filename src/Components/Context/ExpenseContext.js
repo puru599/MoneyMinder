27c22b94b7def5e2fac91a0684babdc2163c1574
SHA43F <shareefpmk44@gmail.com>
2022-11-29 17:22:49 +0530

    6.9) Edit Delete Expenses

--- src/Components/Context/ExpenseContext.js
+++ src/Components/Context/ExpenseContext.js
@@ -4,6 +4,8 @@ const ExpenseContext = React.createContext({
   expenses: null,
   addExpense: () => {},
   getExpense: () => {},
+  deleteExpense: () => {},
+  editExpense: () => {},
 });
 
 export const ExpenseContextProvider = (props) => {
@@ -49,27 +51,80 @@ export const ExpenseContextProvider = (props) => {
           }
         );
         const data = await response.json();
-        const itemsArray = Object.keys(data).map((expense) => {
-          return {
-            id: expense,
-            money: data[expense].money,
-            description: data[expense].description,
-            category: data[expense].category,
-          };
-        });
+        let itemsArray = [];
+        if (!!data) {
+          itemsArray = Object.keys(data).map((expense) => {
+            return {
+              id: expense,
+              money: data[expense].money,
+              description: data[expense].description,
+              category: data[expense].category,
+            };
+          });
+        }
         setExpenses(itemsArray);
       } catch (error) {
-        alert(error.message);
+        console.log(error.message);
       }
     };
 
     getExpenseItem();
   };
 
+  const deleteExpenseHandler = (id) => {
+    const deleteExpenseItem = async (id) => {
+      try {
+        const response = await fetch(
+          `https://react-expense-tracker-27b38-default-rtdb.firebaseio.com/expenses/${id}.json`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+
+        getExpenseHandler();
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    deleteExpenseItem(id);
+  };
+
+  const editExpenseHandler = (expenseItem) => {
+    const editExpenseItem = async (expenseItem) => {
+      try {
+        const response = await fetch(
+          `https://react-expense-tracker-27b38-default-rtdb.firebaseio.com/expenses/${expenseItem.id}.json`,
+          {
+            method: "PUT",
+            body: JSON.stringify({
+              money: expenseItem.money,
+              description: expenseItem.description,
+              category: expenseItem.category,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        getExpenseHandler();
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    editExpenseItem(expenseItem);
+  };
+
   const expenseContext = {
     expenses: expenses,
     addExpense: addExpenseHandler,
     getExpense: getExpenseHandler,
+    deleteExpense: deleteExpenseHandler,
+    editExpense: editExpenseHandler,
   };
   return (
     <ExpenseContext.Provider value={expenseContext}>
