70db9ecd79509ca8ab37e55ba8d13bc55527a066
SHA43F <shareefpmk44@gmail.com>
2022-12-03 19:33:20 +0530

    6.25) Mocking TDD

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./Components/Store/Store";
 import { BrowserRouter } from "react-router-dom";
-import Expenses from "./Components/Pages/Expenses/Expenses";
+
 
 describe("App Test", () => {
   test("Expense Tracker", () => {
@@ -20,37 +20,22 @@ describe("App Test", () => {
     expect(expenseTracker).toBeInTheDocument();
   });
 
-  test("Expense Tracker", () => {
-    render(
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    );
-    const Logout = screen.getByRole("select");
-    userEvent.click(Logout);
+  // test("Expense Tracker", () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <Provider store={store}>
+  //         <App />
+  //       </Provider>
+  //     </BrowserRouter>
+  //   );
+  //   const Logout = screen.getByRole("select");
+  //   userEvent.click(Logout);
 
-    const ExpensesText = screen.getByText("Other", {
-      exact: false,
-    });
-    expect(ExpensesText).toBeInTheDocument();
-  });
+  //   const ExpensesText = screen.getByText("Other", {
+  //     exact: false,
+  //   });
+  //   expect(ExpensesText).toBeInTheDocument();
+  // });
 
-  test("renders posts if request succeeds", async () => {
-    window.fetch = jest.fn();
-    window.fetch.mockResolvedValueOnce({
-      json: async () => [
-        {
-          money: "expense.money",
-          description: "expense.description",
-          category: "expense.category",
-        },
-      ],
-    });
-    render(<Expenses />);
 
-    const listItemElements = await screen.findAllByRole("listitem");
-    expect(listItemElements).not.toHaveLength(0);
-  });
 });
