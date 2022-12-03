7fc5a04e7acfaed5682f93b8f66a36bec92f0627
SHA43F <shareefpmk44@gmail.com>
2022-12-03 19:15:46 +0530

    6.25)  Async TDD

--- src/App.test.js
+++ src/App.test.js
@@ -19,4 +19,38 @@ describe("App Test", () => {
     });
     expect(expenseTracker).toBeInTheDocument();
   });
+
+  test("Expense Tracker", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    );
+    const Logout = screen.getByRole("select");
+    userEvent.click(Logout);
+
+    const ExpensesText = screen.getByText("Other", {
+      exact: false,
+    });
+    expect(ExpensesText).toBeInTheDocument();
+  });
+
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          money: "expense.money",
+          description: "expense.description",
+          category: "expense.category",
+        },
+      ],
+    });
+    render(<Expenses />);
+
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
+  });
 });
